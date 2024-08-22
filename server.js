e359ab20435317bd83299a600a6425798a428d22
ammargoher94@mail.com <ammargoher94@mail.com>
2024-08-22 16:59:06 +0300

    ammar

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: 'https://food-fronty.vercel.app',
+    origin: ['https://food-fronty.vercel.app', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
