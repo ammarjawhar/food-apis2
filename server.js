f7e18a505f460aa90c0c2f69256d10bf9e0deae1
ammargoher94@mail.com <ammargoher94@mail.com>
2024-08-22 17:05:07 +0300

    m

--- server.js
+++ server.js
@@ -12,13 +12,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 //middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: ['https://food-fronty.vercel.app', 'http://localhost:5173'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  })
-);
+app.use(cors());
 //db config
 connectDb();
 //api routes
